4211eb0b440b226325c0a76fae1dc046891c9534
Hayaki Saito <user@zuse.jp>
2014-04-24 16:12:49 +0900

    Strip trailing spaces

--- tools/update_license.py
+++ tools/update_license.py
@@ -12,7 +12,7 @@ for filename in sys.argv[1:]:
         content = f.read()
         m = re.search(pattern, content)
         if m:
-            sep = m.group(1)
+            sep = m.group(1).rstrip()
             copying = sep + sep.join(open(template_file).readlines())
             content = re.sub(pattern, "\n\\1\\2\n%s\\1\\3" % copying, content, re.MULTILINE)
             out = open(filename, "w")
