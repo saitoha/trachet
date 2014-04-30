3ed7c97f2b2716b9a3db95fc7fb6715d48ce20b8
Hayaki Saito <user@zuse.jp>
2014-04-30 17:41:10 +0900

    Bump version

--- trachet/__init__.py
+++ trachet/__init__.py
@@ -19,7 +19,7 @@
 # ***** END LICENSE BLOCK *****
 
 __author__ = "Hayaki Saito (user@zuse.jp)"
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 __license__ = "GPL v3"
 __doc__ = """
 This program runs as a terminal filter process between terminals and applications.
